d4b4753fbc0e082df1f4b22126c8dc4acd73b0b8
AsipheMhambi <141224293+AsipheMhambi@users.noreply.github.com>
2023-11-29 14:37:57 +0200

    changes

--- contact.js
+++ contact.js
@@ -1,47 +1,10 @@
-// api/contact.js
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-
-const app = express();
-const port = 3000;
-
-// Parse incoming requests with JSON payloads
-app.use(bodyParser.json());
-
-// Endpoint for handling form submissions
-app.post('contact', (req, res) => {
+module.exports = async (req, res) => {
   const { name, email, message } = req.body;
 
-  // Replace these with your email and SMTP details
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'amhambi95@gmail.com',
-      pass: '25a06@M95',
-    },
-  });
+  // Perform any necessary processing (e.g., send email, save to a database)
 
-  const mailOptions = {
-    from: email,
-    to: 'amhambi95@gmail.com',
-    subject: 'New Contact Form Submission',
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-  };
+  // For demonstration purposes, log the form data
+  console.log('Form Data:', { name, email, message });
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    } else {
-      console.log('Email sent: ' + info.response);
-      res.status(200).send('OK');
-    }
-  });
-});
-
-// Listen to requests
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  return res.status(200).json({ message: 'Form submitted successfully!' });
+};
